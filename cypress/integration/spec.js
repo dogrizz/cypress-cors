8d98eecfeff03e772a2c0faea2d4ff309e27634a
Dominik Zmitrowicz <dogrizz@gmail.com>
2021-05-21 20:26:20 +0200

    Code to reproduce cors error

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('breaks on cors', () => {
+    cy.visit('/')
+    cy.get('#cors').click()
+    cy.location('href').should('contain','cors')
+  })
+
+  it('should work', () => {
+    cy.visit('/')
+    cy.get('#works').click()
+    cy.location('href').should('contain','works')
   })
 })
